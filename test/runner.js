079fe3ec2bd1b8407177a7f36469204f46f85a18
Martin Feckie <mfeckie@gmail.com>
2014-04-22 12:04:27 +0800

    Ensure test examples are loaded

--- test/runner.js
+++ test/runner.js
@@ -5,7 +5,8 @@ if (window.location.search.indexOf("?test") !== -1) {
           '<link rel="stylesheet" href="test/stylesheets/qunit.css">' +
           '<script src="test/lib/qunit.js"></script>' +
           '<script src="test/initialTests.js"></script>' +
-          '<script src="test/leapYearTests.js"></script>'
-
+          '<script src="test/leapYearTests.js"></script>' +
+          '<script src="test/fizzBuzzTests.js"></script>' +
+          '<script src="test/playingCardsTest.js"></script>'
   )
 }
